8105a209ddcbf1d577c96caae860c02583184806
wrachel <Rwei951@gmail.com>
2022-05-12 18:36:14 -0700

    finishing seater algorithm

--- src/main/java/seater.java
+++ src/main/java/seater.java
@@ -12,7 +12,7 @@ public class seater {
     private int numStudents;
     private int studentsPerTable;
 
-    public seater(int numTables, int numStudents, int studentsPerTable, String students){
+    public seater(int numTables, int numStudents, int studentsPerTable, String students) {
         this.numTables = numTables;
         this.numStudents = numStudents;
         this.studentsPerTable = studentsPerTable;
@@ -21,7 +21,7 @@ public class seater {
         this.names = new ArrayList<String>(Arrays.asList(split));
     }
 
-    public seater(int numTables, int numStudents, String students){
+    public seater(int numTables, int numStudents, String students) {
         this.numTables = numTables;
         this.numStudents = numStudents;
 
@@ -30,17 +30,58 @@ public class seater {
         this.names = new ArrayList<String>(Arrays.asList(split));
     }
 
-    public ArrayList<Student> randomize(){
+    public ArrayList<Student> randomize() {
         ArrayList<Student> groups = new ArrayList<Student>();
 
-        for(int i = 0; i < numTables; i ++){
-            for(int j = 0; j < studentsPerTable; j++){
+        for (int i = 0; i < numTables; i++) {
+            for (int j = 0; j < studentsPerTable; j++) {
                 int index = (int) (Math.random() * names.size());
-               // tableGroups[i][j] = names.get(index);
+                Student oneStudent = new Student(i, names.get(index));
+                groups.add(oneStudent);
+                names.remove(index);
             }
         }
-
         return groups;
     }
+    //tester method
+    public static void main(String args[]){
+        seater test = new seater(5, 10, 2, "Harry, Rachel, Calvin, Devam, Yajat, Harry2, Rachel2, Calvin2, Devam2, Yajat2");
+
+        System.out.print("All student names: ");
+        for(String a : test.names){
+            System.out.println(a + " ");
+        }
+
+        ArrayList<Student> newGroups = test.randomize();
+
+        System.out.print("New groups: ");
+        for(Student b : newGroups){
+            System.out.println("Table: " + b.getTableGroup() + ", Name: " + b.getStudentName());
+        }
+    }
 }
+    class Student{
+        private int tableGroup;
+        private String studentName;
+
+        public Student(int a, String str){
+            this.studentName = str;
+            this.tableGroup = a;
+        }
+
+        public String getStudentName(){
+            return studentName;
+        }
+        public int getTableGroup(){
+            return tableGroup;
+        }
+        public void setStudentName(String s){
+            studentName = s;
+        }
+        public void setTableGroup(int a){
+            tableGroup = a;
+        }
+    }
+
+
 
